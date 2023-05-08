32622ac726f5092182006f88f6641ebe67bcab26
Garg_ Shobhit (shogarg@deloitte.com) <shogarg@your.hostname.com>
2023-05-08 14:52:14 +0530

    ignite app using parcel and install react as npm library

---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,19 @@
+const parent = React.createElement("div", {
+    id: "parent"
+}, React.createElement("div", {}, [
+    React.createElement("h1", {}, "Hey I am nested h1 tag "),
+    React.createElement("h1", {}, "Hey I am sibling inside parent container ")
+]));
+const heading = React.createElement("h1", {
+    id: "heading"
+}, "Hello World from React");
+// React.createElement returns JS object.
+// Heading is an object consisting props.Check in console.
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// heading is just an object yet
+// Render method is basically responsible for take this object and convert it into the heading tag and put it upon the DOM
+// root.render(heading);
+root.render(parent);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
