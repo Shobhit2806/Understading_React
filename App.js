cc4ab9ed6630f3bc2870765b40ae4e031d5cc12d
Garg_ Shobhit (shogarg@deloitte.com) <shogarg@your.hostname.com>
2023-08-02 10:19:28 +0530

    minor changes

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent",class:"parentClass" },
+  React.createElement("div", {}, [
+    React.createElement("h1", {}, "Hey I am nested h1 tag "),
+    React.createElement("h1", {}, "Hey I am sibling inside parent container "),
+  ])
+); 
 
-
-
-const parent = React.createElement("div", { id: "parent" },
-    React.createElement("div", {},
-        [React.createElement("h1", {}, "Hey I am nested h1 tag "),
-        React.createElement("h1", {}, "Hey I am sibling inside parent container ")]))
-
-
-
-
-const heading = React.createElement("h1", { id: "heading" }, "Hello World from React");
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from React"
+);
 
 // React.createElement returns JS object.
 // Heading is an object consisting props.Check in console.
 
-console.log(heading)
+// See it will give obj in console not html element
+console.log(heading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
